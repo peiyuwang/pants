7f1ef508baf6d520f4ba2bf073e853b1ff354b04
Peiyu Wang <wangpeiyu@gmail.com>
2016-04-21 20:33:11 -0700

    crazy

--- src/python/pants/engine/exp/storage.py
+++ src/python/pants/engine/exp/storage.py
@@ -189,7 +189,7 @@ class Storage(Closable):
     self._contents.put(key.digest, blob)
 
     # have to sample travis only allow 4Mb log file
-    if 'SelectNode' in str(key):
+    if '[(SelectNode' in str(key):
       sys.stderr.write('Under: {}\n'.format(key))
       sys.stderr.write('Retrieved back: {}\n'.format(self.get(key)))
     assert self.get(key) == obj
