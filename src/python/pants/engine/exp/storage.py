8e7a4b2ec415b574fedb8d33b75245f5b31d0b4f
Peiyu Wang <wangpeiyu@gmail.com>
2016-04-22 18:55:12 -0700

    StringIO + pickle + loop 4 times

--- src/python/pants/engine/exp/storage.py
+++ src/python/pants/engine/exp/storage.py
@@ -5,8 +5,8 @@
 from __future__ import (absolute_import, division, generators, nested_scopes, print_function,
                         unicode_literals, with_statement)
 
-import cPickle as pickle
 import logging
+import pickle
 import StringIO
 import sys
 from abc import abstractmethod
